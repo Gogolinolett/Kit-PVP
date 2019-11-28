8ed30b1dd662047273683f32a5376f2529fcf555
Sebastian Frenz <sebastian.daniel.frenz@gmail.com>
2019-11-28 18:59:27 +0100

    part 1 (saving)

--- src/io/github/Gogolinolett/WW1Job/SimpleDBMTadapter/DBItemStack.java
+++ src/io/github/Gogolinolett/WW1Job/SimpleDBMTadapter/DBItemStack.java
@@ -1,47 +1,67 @@
 package io.github.Gogolinolett.WW1Job.SimpleDBMTadapter;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang.NotImplementedException;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.Gogolinolett.WW1Job.Utils;
 import io.github.SebastianDanielFrenz.SimpleDBMT.varTypes.DBCompareable;
 import io.github.SebastianDanielFrenz.SimpleDBMT.varTypes.DBvalue;
 
 public class DBItemStack implements DBvalue {
 
-	private ItemStack itemstackValue;
-	
+	private ItemStack value;
+
 	@Override
 	public void Parse(String text) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
+		Map<String, Object> map = (Map<String, Object>) Arrays.asList(text.split(",")).stream().map(s -> s.split(":"))
+				.collect(Collectors.toMap(e -> e[0], e -> {
+					try {
+						return Utils.deserialize(e[1]);
+					} catch (ClassNotFoundException | IOException e1) {
+						throw new RuntimeException("Could not deserialize string: \"" + e[1] + "\"");
+					}
+				}));
+		value = ItemStack.deserialize(map);
 	}
 
 	@Override
 	public String Save() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> map = value.serialize();
+		String out = "";
+
+		String[] keys = (String[]) map.keySet().toArray();
+		Object[] values = map.values().toArray();
+		for (int i = 0; i < map.size() - 1; i++) {
+			out += keys[i] + "=" + String.valueOf(values[i]) + ",";
+		}
+		if (map.size() != 0) {
+			out += keys[map.size() - 1] + "=" + String.valueOf(values[map.size() - 1]);
+		}
+		return out;
 	}
 
 	@Override
 	public boolean Equals(DBCompareable value2) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new NotImplementedException(
+				"Ha ha... nein. Du hast die Methode doch noch gar nicht implementiert *facepalm*");
 	}
 
 	@Override
 	public String Display() {
-		// TODO Auto-generated method stub
-		return null;
+		return Save();
 	}
 
-	public ItemStack getItemstackValue() {
-		return itemstackValue;
+	public ItemStack getValue() {
+		return value;
 	}
 
-	public void setItemstackValue(ItemStack itemstackValue) {
-		this.itemstackValue = itemstackValue;
+	public void setValue(ItemStack value) {
+		this.value = value;
 	}
 
 }

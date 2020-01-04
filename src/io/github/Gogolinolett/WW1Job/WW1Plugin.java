c2cae675a2ea07dc8d095f5569d98a134f7d4f13
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2020-01-04 18:03:23 +0100

    updating

--- src/io/github/Gogolinolett/WW1Job/WW1Plugin.java
+++ src/io/github/Gogolinolett/WW1Job/WW1Plugin.java
@@ -1,6 +1,13 @@
 package io.github.Gogolinolett.WW1Job;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -8,32 +15,19 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import io.github.Gogolinolett.WW1Job.SimpleDBMTadapter.DBLocation;
-import io.github.SebastianDanielFrenz.SimpleDBMT.CrashedDBstock;
-import io.github.SebastianDanielFrenz.SimpleDBMT.DataBaseHandler;
-import io.github.SebastianDanielFrenz.SimpleDBMT.query.DataBaseQuery;
-import io.github.SebastianDanielFrenz.SimpleDBMT.query.DefaultDataBaseQuery;
-import io.github.SebastianDanielFrenz.SimpleDBMT.query.QueryResult;
-import io.github.SebastianDanielFrenz.SimpleDBMT.query.SearchedValue;
-import io.github.SebastianDanielFrenz.SimpleDBMT.registry.TypeRegistry;
-import io.github.SebastianDanielFrenz.SimpleDBMT.varTypes.DBString;
-import io.github.SebastianDanielFrenz.SimpleDBMT.varTypes.DBint;
 
 public class WW1Plugin extends JavaPlugin {
 
 	public static WW1Plugin plugin;
-	public static DataBaseHandler dbh;
-	public static DataBaseQuery dataBaseQuery;
+	public static Connection sqlc;
+
 
 	public void onEnable() {
 		plugin = this;
 
 		setupDB();
-		dataBaseQuery = new DefaultDataBaseQuery(dbh);
 
-		TypeRegistry reg = CrashedDBstock.getDefaultTypeRegistry();
-		reg.register(DBLocation.class);
-
+		
 		try {
 			dbh.addDataBase("WW1Job.db", reg);
 		} catch (IOException e) {
@@ -73,13 +67,14 @@ public class WW1Plugin extends JavaPlugin {
 	
 	public static void setPlayerTeam(Player player, int team){
 		
+		runSQL("UPDATE Players SET Team \""+ team +"\" WHERE UUID =\"" +player.getUniqueId().toString() +"\" ");
 	dataBaseQuery.Update("WW1Job", "Players", new SearchedValue[] {new SearchedValue("UUID", new DBString(player.getUniqueId().toString()))  }  ,
 			new SearchedValue[] {new SearchedValue("Team", new DBint(team)) });	
 		
 	}
 
 	public static Location getStandardSpawn() {
-
+		runSQL("");
 		QueryResult queryResult = dataBaseQuery.Run("WW1Job", "Standard", new String[] { "Location" },
 				new SearchedValue[] { new SearchedValue("Location", new DBString("")) });
 
@@ -146,31 +141,42 @@ public class WW1Plugin extends JavaPlugin {
 
 	}
 
-	public boolean setupDB() {
-		RegisteredServiceProvider<DataBaseHandler> dataBaseProvider = getServer().getServicesManager()
-				.getRegistration(DataBaseHandler.class);
-		if (dataBaseProvider != null) {
-			dbh = dataBaseProvider.getProvider();
-		}
+	public void setupDB() {
+		boolean isNew = Files.exists(Paths.get(getDataFolder().getAbsolutePath() + "/database.db"));
+		sqlc = connect(getDataFolder().getAbsolutePath() + "/database.db");
 
-		return dataBaseProvider != null;
+		if (isNew) {
+			runSQL("CREATE TABLE Locations (world1 STRING , x1 REAL, y1 REAL, z1 REAL, world2 STRING, x2 REAL, y2 REAL , z2 REAL,name STRING PRIMARY KEY); CREATE TABLE Players(Map STRING, Team REAL, UUID STRING PRIMARY KEY); CREATE TABLE Standard(world STRING, x REAL, y REAL, z REAL)");
+		}
 	}
 
-	public static void setMapSpawn(String name, Player player, int team) {
-
+	public static void setMapSpawn(String name, Player player, int team)  {
+		
 		if (3 > team && team > 0) {
-			dataBaseQuery.Update("WW1Job", "Locations",
+			
+			runSQL("UPDATE Locations SET world"+ team +" = \"" + player.getWorld().getName() + "\",x"+ team +"=" + player.getLocation().getX()
+					+ ",y"+ team +"=" + player.getLocation().getY() + ",z"+ team +"=" + player.getLocation().getZ());
+			
+			
+			
+			/* dataBaseQuery.Update("WW1Job", "Locations",
 					new SearchedValue[] { new SearchedValue("name", new DBString(name)) },
 					new SearchedValue[] { new SearchedValue("location" + team, new DBLocation(player.getLocation())) });
 
 			QueryResult queryResult = dataBaseQuery.Run("WW1Job", "Locations", new String[] { "name" },
 					new SearchedValue[] { new SearchedValue("name", new DBString(name)) });
-
-			if (queryResult.isEmpty()) {
-
+					*/
+			
+			try{
+				if (!runSQL("SELECT world"+ team +"FROM Locations WHERE name = \"" + name + "\"")){
+					player.sendMessage("This Map does not exist!");
+				}
+			}catch(Exception e){
 				player.sendMessage("This Map does not exist!");
-
 			}
+			
+
+			
 		} else {
 			player.sendMessage("This team does not exist");
 		}
@@ -178,16 +184,15 @@ public class WW1Plugin extends JavaPlugin {
 	}
 
 	public static void createMap(String name, Player player) {
-
-		QueryResult queryResult = dataBaseQuery.Run("WW1Job", "Locations", new String[] { "name" },
-				new SearchedValue[] { new SearchedValue("name", new DBString(name)) });
+		
+		
+		ResultSet rs = runSQL("SELECT name from");
 
 		if (queryResult.isEmpty()) {
-
-			dataBaseQuery.Insert("WW1Job", "Locations",
-					new SearchedValue[] { new SearchedValue("name", new DBString(name)),
-							new SearchedValue("location1", new DBLocation(null)),
-							new SearchedValue("location2", new DBLocation(null)) });
+			
+			
+			runSQL("INSERT INTO Locations (name) VALUES(\""+ name + "\" )");
+			
 		} else {
 			player.sendMessage("This Map exists! Please Choose a different name!");
 
@@ -211,5 +216,26 @@ public class WW1Plugin extends JavaPlugin {
 
 		return location;
 	}
+	
+	public static Connection connect(String path) {
+		// SQLite connection string
+		String url = "jdbc:sqlite:" + path;
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+
+	public static ResultSet runSQL(String sql) {
+		try {
+			return sqlc.createStatement().executeQuery(sql);
+		} catch (SQLException e) {
+			throw new RuntimeException("See SQLException");
+		}
+	}
+
 
 }
